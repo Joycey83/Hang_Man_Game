deed907fafadc903f732c87caa89ffda599e678f
Joycey <Jcheung8385@gmail.com>
2021-12-29 13:24:28 +0000

    Add word list to my hangman game

--- run.py
+++ run.py
@@ -11,4 +11,9 @@ print("You will only have 9 lives to get the word correct")
 time.sleep(2)
 print("Game begins now...\n")
 time.sleep(2)
+words = ['act', 'air', 'age', 'bag', 'cap', 'map', 
+         'area', 'baby', 'card', 'dish', 'exam', 
+         'good', 'boards', 'chair', 'count', 'facts',
+         'green', 'house']
+
 
