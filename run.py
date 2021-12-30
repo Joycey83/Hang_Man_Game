0b63502bcb4fdbd0f67870a0f9b1dac759df4851
Joycey <Jcheung8385@gmail.com>
2021-12-30 11:43:04 +0000

    Add \n to all my input method so that it will show on the heroku terminal, if not added it will not show up

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ import random
 import time
 
 # Users input their names before starting to play the game
-name = input("ENTER YOU NAME -> ")
+name = input("ENTER YOU NAME ->\n ")
 print("Welcome to Hangman " + name + "! Best of Luck...")
 print("You will only have 9 lives to get the word correct")
 time.sleep(2)
@@ -52,13 +52,13 @@ while NUMBER_MISTAKES < NUMBER_MISTAKES_ALLOWED:
         print("{}, ".format(letter), end='')
     print()
     print("Guesses left: {}".format(NUMBER_MISTAKES_ALLOWED - NUMBER_MISTAKES))
-    letter_user = input('Enter a letter -->')  
+    letter_user = input('Enter a letter -->\n')  
 # checking if the letter has been guessed before
 
     while letter_user in letters_guessed or letter_user in wrong_letters:
         print()
         print('You have already USED this letter, enter another one please')
-        letter_user = input('Enter a letter --> ')
+        letter_user = input('Enter a letter -->\n ')
 # if the letter not in the word the wrong letter will be printed for the player
     if letter_user not in letters_word:
         NUMBER_MISTAKES += 1
