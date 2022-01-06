cd9d8fdf1d8cae0c4c5bbec518a324c989bb9f4f
Joycey <Jcheung8385@gmail.com>
2022-01-06 11:49:44 +0000

    Add existing feature heading to my readme file

--- run2.py
+++ run2.py
@@ -16,6 +16,7 @@ letters_word = list(word)
 wrong_letters = []
 
 
+
 def validate_input_no_empty(input):
     # Validate is not empty
     if not input:
@@ -62,63 +63,14 @@ def get_guess(already_guessed):
             return guess
 
 
-while NUMBER_MISTAKES < NUMBER_MISTAKES_ALLOWED:
-    print()
-    print('Wrong letters: ', end='')
-    for letter in wrong_letters:
-        print("{}, ".format(letter), end='')
-    print()
-    print("Guesses left: {}".format(NUMBER_MISTAKES_ALLOWED - NUMBER_MISTAKES))
-    letter_user = input('Enter a letter -->\n')  
-# checking if the letter has been guessed before
-
-    while letter_user in letters_guessed or letter_user in wrong_letters:
-        print()
-        print('You have already USED this letter, enter another one please')
-        letter_user = input('Enter a letter -->\n ')
-# if the letter not in the word the wrong letter will be printed for the player
-    if letter_user not in letters_word:
-        NUMBER_MISTAKES += 1
-        wrong_letters.append(letter_user)
-    
-    print()
-    print('Word: ', end='')
-# If the letter is correct it will replace the blanks in the correct space
-    for letter in letters_word:
-        if letter_user == letter:
-            letters_guessed.append(letter_user)
-
-    for letter in letters_word:
-        if letter in letters_guessed:
-            print(letter + ' ', end='')
-        else:
-            print('_ ', end='')
-
-# Hangman graphics correlate with amount of mistakes made
-    print()
-    if NUMBER_MISTAKES:
-        print(HANGMAN_GRAPHICS[NUMBER_MISTAKES - 1])
-    print()
-    print('-------------------------------------------')
-# Word guessed correctly so player wins
-    if len(letters_guessed) == len(letters_word):
-        print()
-        print("Congratulations! You have WOOON!!") 
-        break
-    
-# Word guessed incorrectly so player lose
-if NUMBER_MISTAKES == NUMBER_MISTAKES_ALLOWED:
-    print()
-    print("OH NOO!!! You are HANGED in the gallows!!")   
-
-
 def play_game(username, word, letters_word):
 
     # Amount of letters the word has
     print()
     print('The word has {} letters'.format(len(letters_word)))
     print_word_hidden(word)
-
+    
 
 if __name__ == "__main__":
     start_game()
+   
